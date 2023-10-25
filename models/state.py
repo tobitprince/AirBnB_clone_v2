45245e8576ebd1e2c4824be71e105c31502894f5
tobitprince <prince.tobit@strathmore.edu>
2023-10-26 01:09:34 +0300

    circular

--- models/state.py
+++ models/state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models import storage_type
 from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
