b3e49fbaab9d4ce10ae8ce42c8a7bc860b5a322f
tobitprince <prince.tobit@strathmore.edu>
2023-10-25 21:59:01 +0300

    pep8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-""" Start a Flash Web Application. """
-from flask import Flask 
+"""Start a Flash Web Application."""
+from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ Print a Message when / is called. """
+    """Print a Message when / is called."""
     return 'Hello HBNB!'
+
 
 if __name__ == "__main__":
     """ Main Function ."""
