c63b416733bc152c67334e1151acd1e362720772
tobitprince <prince.tobit@strathmore.edu>
2023-10-26 01:12:32 +0300

    storage_engine

--- models/city.py
+++ models/city.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models import storage_type
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
+
+storage_engine = getenv("HBNB_TYPE_STORAGE")
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    if storage_type == 'db':
+    if storage_engine == 'db':
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         places = relationship('Place', backref='cities',
