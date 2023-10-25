b6440016722e22b9e15fbeac92f057cfd2d4ae81
tobitprince <prince.tobit@strathmore.edu>
2023-10-26 01:11:00 +0300

    circulara

--- models/state.py
+++ models/state.py
@@ -4,12 +4,15 @@ from models.base_model import BaseModel, Base
 from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
+
+storage_engine = getenv("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
     """ State class / table model"""
     __tablename__ = 'states'
-    if storage_type == 'db':
+    if storage_engine == 'db':
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state',
                               cascade='all, delete, delete-orphan')
